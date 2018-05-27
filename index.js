f42870e858932351fe65475c6cabbf846ad44bae
Alex Scarlett <alex.scarlett91@gmail.com>
2018-05-27 19:52:05 -0400

    Debugging

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+// const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT || 5000);
