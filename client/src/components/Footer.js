a9f08be503a940a1503ec1d74f92f155e2e07008
Alex Scarlett <alex.scarlett91@gmail.com>
2018-05-27 02:13:01 -0400

    Question mark

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer-no-sticky">
-      <div className="footer-text">© 2018 How's Your App</div>
+      <div className="footer-text">© 2018 How's Your App?</div>
     </footer>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -34,7 +34,7 @@ class Header extends Component {
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
-            How's Your App
+            How's Your App?
           </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
