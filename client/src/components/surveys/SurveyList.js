f0f98428adafe6f58478db986195635271ca18ab
Alex Scarlett <alex.scarlett91@gmail.com>
2018-06-04 17:18:12 -0400

    Change survey display

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -10,20 +10,24 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
-        <div className="card grey lighten-5" key={survey._id}>
-          <div className="card-content">
-            <span className="card-title">{survey.title}</span>
-            <p className="card-p">{survey.body}</p>
-            <p className="right card-date">
-              Sent:
-              <span style={{ paddingLeft: '10px' }}>
-                {new Date(survey.dateSent).toLocaleDateString()}
-              </span>
-            </p>
-          </div>
-          <div className="card-action">
-            <a>Yes: {survey.yes}</a>
-            <a>No: {survey.no}</a>
+        <div className="row">
+          <div className="col s8 offset-2">
+            <div className="card grey lighten-5" key={survey._id}>
+              <div className="card-content">
+                <span className="card-title">{survey.title}</span>
+                <p className="card-p">{survey.body}</p>
+                <p className="right card-date">
+                  Sent:
+                  <span style={{ paddingLeft: '10px' }}>
+                    {new Date(survey.dateSent).toLocaleDateString()}
+                  </span>
+                </p>
+              </div>
+              <div className="card-action">
+                <a>Yes: {survey.yes}</a>
+                <a>No: {survey.no}</a>
+              </div>
+            </div>
           </div>
         </div>
       );
